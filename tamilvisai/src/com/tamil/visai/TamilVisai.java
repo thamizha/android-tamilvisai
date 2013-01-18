a3d0fc2e378a7a2b40d8a24ffbe0c96644d33084
Rajeshwaran S P <tuxrajesh@gmail.com>
2013-01-18 00:28:35 -0500

    Improvements done on UI and usability front
    1. Added a new button to launch the language and input settings.
    2. Changed the UI on the tabs to be more elegant.

--- tamilvisai/src/com/tamil/visai/TamilVisai.java
+++ tamilvisai/src/com/tamil/visai/TamilVisai.java
@@ -1,49 +1,78 @@
 package com.tamil.visai;
 
+import java.util.List;
+
 import android.app.Activity;
-import android.graphics.Color;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
-import android.widget.LinearLayout;
+import android.provider.Settings;
+import android.view.View;
+import android.view.inputmethod.InputMethodInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class TamilVisai extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tamilvisai);
-        Typeface fontface = Typeface.createFromAsset(getAssets(), "fonts/mylai.ttf");        
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tamilvisai);
+		
+		Typeface fontface = Typeface.createFromAsset(getAssets(),
+				"fonts/mylai.ttf");
+		
+		TextView headerText = (TextView) findViewById(R.id.header_text);
+		headerText.setTypeface(fontface, Typeface.BOLD);
+		headerText.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_Header)));
 
-        TextView appHeader = (TextView) findViewById(R.id.App_header);
-        appHeader.setTypeface(fontface,Typeface.BOLD);
-        appHeader.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_Header)));
-        
-        String urltxt = getResources().getString(R.string.thamizha_url);
-        String url = new StringBuilder("<a href = \"")
-        					.append(urltxt)
-        					.append("\">")
-        					.append(urltxt)
-        					.append("</a>").toString();
-        
-        
-        
-        TextView footerText = (TextView) findViewById(R.id.Footer_txt);
-        footerText.setTypeface(fontface,Typeface.BOLD);     
-        footerText.setTextSize(20);
-        footerText.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.thamizha_txt)));
-        footerText.setTextColor(Color.WHITE);
-        
-        TextView footerUrl = (TextView) findViewById(R.id.Footer_url);
-        footerUrl.setMovementMethod(LinkMovementMethod.getInstance());
-        footerUrl.setTextColor(Color.WHITE);
-        footerUrl.setText(Html.fromHtml(url));
-        
-        TextView welcomeTxt =(TextView) findViewById(R.id.Welcome_text);
-        welcomeTxt.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_welcome)));
-        welcomeTxt.setTypeface(fontface,Typeface.BOLD);
-        welcomeTxt.setTextColor(Color.WHITE);
-        
-    }
+		TextView welcomeText = (TextView) findViewById(R.id.welcome_text);
+		welcomeText.setTypeface(fontface, Typeface.NORMAL);
+		welcomeText.setText(UnicodeUtil.unicode2tsc(getResources().getString(
+				R.string.text_welcome)));
+		
+		Button enableButton = (Button) findViewById(R.id.enable_tamilvisai);
+		enableButton.setTypeface(fontface, Typeface.NORMAL);
+		enableButton.setText(UnicodeUtil.unicode2tsc(getResources()
+				.getString(R.string.text_enable_visai)));
+		
+		TextView contactText = (TextView) findViewById(R.id.contact_text);
+		contactText.setTypeface(fontface, Typeface.ITALIC);
+		String urltxt = getResources().getString(R.string.thamizha_url);
+		String url = new StringBuilder(UnicodeUtil.unicode2tsc(getResources()
+				.getString(R.string.thamizha_txt))).append("  ").append(urltxt).toString();
+		contactText.setText(url);
+		
+		// Check if TamilVisai is enabled
+		InputMethodManager inputManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+		List<InputMethodInfo> enabledInputMethods = inputManager
+				.getEnabledInputMethodList();
+
+		boolean visaiEnabled = false;
+		for (InputMethodInfo imInfo : enabledInputMethods) {
+			if (imInfo.getId().equalsIgnoreCase(
+					"com.tamil.visai/.TamilSoftKeyboard")) {
+				visaiEnabled = true;
+				break;
+			}
+		}
+		
+		// if enabled, don't show button to enable
+		if (visaiEnabled) {
+			//enableButton.setVisibility(View.GONE);
+		}
+	}
+
+	/**
+	 * btnEnable_Click : show the input settings to enable TamilVisai
+	 * 
+	 * @param view
+	 */
+	public void btnEnable_Click(View view) {
+		Intent enableInputMethodIntent = new Intent(
+				Settings.ACTION_INPUT_METHOD_SETTINGS);
+		enableInputMethodIntent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+		enableInputMethodIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		getApplicationContext().startActivity(enableInputMethodIntent);
+	}
 }
--- tamilvisai/src/com/tamil/visai/TamilvisaiHelpHome.java
+++ tamilvisai/src/com/tamil/visai/TamilvisaiHelpHome.java
@@ -1,27 +1,31 @@
 package com.tamil.visai;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class TamilvisaiHelpHome extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tamilvisai_help_home);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tamilvisai_help_home);
+		
+		Typeface fontface = Typeface.createFromAsset(getAssets(),
+				"fonts/mylai.ttf");
 
-    TextView helpHdr =(TextView) findViewById(R.id.Help_header);
-    helpHdr.setText(R.string.text_help_header);
-    helpHdr.setTypeface(helpHdr.getTypeface(), Typeface.BOLD);
-    helpHdr.setTextColor(Color.BLACK);
-    TextView helpSteps =(TextView) findViewById(R.id.Help_steps);
-    helpSteps.setText(R.string.text_help_steps);
-    helpSteps.setTextColor(Color.BLACK);
-    
-    LinearLayout layout = (LinearLayout)findViewById(R.id.helpWrapper);
-    layout.setBackgroundColor(Color.WHITE);
-    }
+		TextView helpStep1 = (TextView) findViewById(R.id.help_step_1);
+		helpStep1.setTypeface(fontface, Typeface.NORMAL);
+		helpStep1.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_help_step_1)));
+		
+		TextView helpStep2 = (TextView) findViewById(R.id.help_step_2);
+		helpStep2.setTypeface(fontface, Typeface.NORMAL);
+		helpStep2.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_help_step_2)));
+		
+		TextView helpStep3 = (TextView) findViewById(R.id.help_step_3);
+		helpStep3.setTypeface(fontface, Typeface.NORMAL);
+		helpStep3.setText(UnicodeUtil.unicode2tsc(getResources().getString(R.string.text_help_step_3)));
+	}
+
+	
 }
